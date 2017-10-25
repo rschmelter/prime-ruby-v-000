6bd2fc241e67e25f445c0bad74fcbe4d33dc475c
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-25 22:56:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@
 def prime?(integer)
   range = (2..integer - 1).to_a
   range.any? do |num|
-    if integer % num.include(.)
+    if integer % num .include(.)
       return false
     else
       return true
     end
-
+    
   end
 
 end
